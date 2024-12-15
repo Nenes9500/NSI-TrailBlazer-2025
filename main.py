a0d32ede102462b15b28d12070d2fc58a02a1729
Nenes9500 <122723239+Nenes9500@users.noreply.github.com>
2024-12-15 19:21:13 +0100

    circuit + camera
    Added a camera following the car, as well as a circuit background

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from game import Game
+import pygame
 from pygame import *
 
 # colors
@@ -11,21 +13,65 @@ cyan = (0, 255, 255)
 pink = (255, 0, 255)
 
 
-# window
-init()
-screen = display.set_mode(size=(960, 540), flags=RESIZABLE)
-display.set_caption("")
-Font = font.SysFont("monospace", 15)
-running = True
-clock = time.Clock()
-screen.fill(green)
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+WINDOW_SURFACE = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE
 
-cars = sprite.Group()
-while running:
-    for Event in event.get():
-        if Event.type == QUIT:
-            running = False
-    display.flip()
-    clock.tick(60)
 
-quit()
+pygame.init()
+pygame.mixer.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_SURFACE)
+pygame.display.set_caption("TrailBlazer")
+
+
+road_image = road_image = pygame.image.load('img/racetrack.png')
+background = road_image
+car_img = pygame.image.load('img/voiture.png').convert_alpha()
+
+
+car = Game(car_img, WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+car_sprites = pygame.sprite.Group()
+car_sprites.add(car.player)
+
+
+clock = pygame.time.Clock()
+done = False
+last_command = None
+while not done:
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            done = True
+        elif (event.type == pygame.VIDEORESIZE):
+            WINDOW_WIDTH = event.w
+            WINDOW_HEIGHT = event.h
+            window = pygame.display.set_mode(
+                (WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_SURFACE)
+            background = road_image
+        elif event.type == pygame.KEYDOWN:
+            car.pressed[event.key] = True
+        elif event.type == pygame.KEYUP:
+            car.pressed[event.key] = False
+
+    if car.pressed.get(pygame.K_DOWN):
+        car.player.brake(0.1)
+    else:
+        car.player.brake(-0.075)
+    if car.pressed.get(pygame.K_UP):
+        car.player.accelerate(0.1)
+    else:
+        car.player.accelerate(-0.075)
+    if car.pressed.get(pygame.K_LEFT):
+        car.player.turn(-1.8)
+    if car.pressed.get(pygame.K_RIGHT):
+        car.player.turn(1.8)
+
+    car_sprites.update()
+
+    window.blit(background, (0, 0))
+    car_sprites.draw(window)
+    pygame.display.flip()
+
+    clock.tick_busy_loop(60)
+
+pygame.quit()
