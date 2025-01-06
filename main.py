0a23b6c497a44ae69076e6aa7b2d0bfd99cea206
Th3scorpio <d.th3scorpio@gmail.com>
2025-01-06 19:32:32 +0100

    Controller v1.0
    Ad joystick with some bug and button.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from pygame import *
+import pygame
 import pygame
 from game import Game
+# from manette import *
 
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
@@ -8,6 +9,12 @@ WINDOW_SURFACE = pygame.HWSURFACE | pygame.NOFRAME | pygame.RESIZABLE
 
 
 pygame.init()
+pygame.joystick.init()
+
+joysticks = pygame.joystick.Joystick(0)
+joysticks.init()
+print(f"Joystick detected: {joysticks.get_name()}")
+
 window = pygame.display.set_mode(
     (WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_SURFACE, vsync=1)
 pygame.display.set_caption("TrailBlazer")
@@ -39,28 +46,33 @@ while not done:
             car.pressed[event.key] = True
         elif event.type == pygame.KEYUP:
             car.pressed[event.key] = False
+        if event.type == pygame.JOYAXISMOTION:
+           if event.axis == 0 and (event.value >0.1 or event.value <-0.1):
+                car.player.turn(1.8)
 
-    if car.pressed.get(pygame.K_DOWN):
+    if car.pressed.get(pygame.K_DOWN) or joysticks.get_button(4):
         car.player.accelerate(-0.1)
     else:
         if car.player.speed < 0:
             car.player.brake(-0.1)
-    if car.pressed.get(pygame.K_UP):
+    if car.pressed.get(pygame.K_UP) or joysticks.get_button(5):
         car.player.accelerate(0.1)
     else:
         if car.player.speed > 0:
             car.player.brake(0.1)
-    if car.pressed.get(pygame.K_SPACE):
+    if car.pressed.get(pygame.K_SPACE) or joysticks.get_button(2):
         if car.player.speed < 0:
             car.player.brake(-0.5)
         if car.player.speed > 0:
             car.player.brake(0.5)
-    if car.pressed.get(pygame.K_LEFT):
+
+    if car.pressed.get(pygame.K_LEFT) or joysticks.get_button(13):
         car.player.turn(-1.8)
-    if car.pressed.get(pygame.K_RIGHT):
+    if car.pressed.get(pygame.K_RIGHT) or joysticks.get_button(14):
         car.player.turn(1.8)
+
+
     car_sprites.update()
-    print(car.player.speed)
     window.blit(background, (-car.player.position.x, - car.player.position.y))
     car_sprites.draw(window)
     pygame.display.flip()
