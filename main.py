06b923f74bfa911e77ef3b63d3d408dbc31de546
Th3scorpio <d.th3scorpio@gmail.com>
2025-01-09 19:29:35 +0100

    V2.0 manette
    New joystick and trigger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import pygame
 from game import Game
 # from manette import *
 
@@ -19,6 +18,7 @@ window = pygame.display.set_mode(
     (WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_SURFACE, vsync=1)
 pygame.display.set_caption("TrailBlazer")
 
+
 background = pygame.image.load('img/racetrack.png')
 background_size = background.get_size()
 background = pygame.transform.scale(
@@ -37,6 +37,9 @@ car_sprites.add(car.player)
 clock = pygame.time.Clock()
 done = False
 last_command = None
+joystick_axis_0 = 0
+joystick_axis_4 = 0
+joystick_axis_5 = 0
 while not done:
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -47,15 +50,28 @@ while not done:
         elif event.type == pygame.KEYUP:
             car.pressed[event.key] = False
         if event.type == pygame.JOYAXISMOTION:
-           if event.axis == 0 and (event.value >0.1 or event.value <-0.1):
-                car.player.turn(1.8)
+            if event.axis == 0:
+                joystick_axis_0 = event.value
+            if event.axis == 4 :
+               joystick_axis_4 = event.value
+            if event.axis == 5 :
+               joystick_axis_5 = event.value
 
-    if car.pressed.get(pygame.K_DOWN) or joysticks.get_button(4):
+    if abs(joystick_axis_0) > 0.1: 
+        car.player.turn(1.8 * joystick_axis_0)
+    
+    if abs(joystick_axis_4) > 0.1: 
+        car.player.accelerate(-0.1 * (joystick_axis_4+1))
+
+    if abs(joystick_axis_5) > 0.1: 
+        car.player.accelerate(0.1 * (joystick_axis_5+1))
+
+    if car.pressed.get(pygame.K_DOWN):
         car.player.accelerate(-0.1)
     else:
         if car.player.speed < 0:
             car.player.brake(-0.1)
-    if car.pressed.get(pygame.K_UP) or joysticks.get_button(5):
+    if car.pressed.get(pygame.K_UP):
         car.player.accelerate(0.1)
     else:
         if car.player.speed > 0:
@@ -66,9 +82,9 @@ while not done:
         if car.player.speed > 0:
             car.player.brake(0.5)
 
-    if car.pressed.get(pygame.K_LEFT) or joysticks.get_button(13):
+    if car.pressed.get(pygame.K_LEFT):
         car.player.turn(-1.8)
-    if car.pressed.get(pygame.K_RIGHT) or joysticks.get_button(14):
+    if car.pressed.get(pygame.K_RIGHT):
         car.player.turn(1.8)
 
 
@@ -76,5 +92,5 @@ while not done:
     window.blit(background, (-car.player.position.x, - car.player.position.y))
     car_sprites.draw(window)
     pygame.display.flip()
-
+    
     clock.tick_busy_loop(60)
