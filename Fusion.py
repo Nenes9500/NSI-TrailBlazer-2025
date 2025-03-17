b806f39677de6970d8459aa00c39554e5113a19d
Th3scorpio <d.th3scorpio@gmail.com>
2025-03-17 22:06:05 +0100

    fusion fin
    Fin de la class fusion et ajout des images pour les circuit

--- Fusion.py
+++ Fusion.py
@@ -1,24 +1,26 @@
 from PIL import Image
-
+larg=512*4
+hauteur=512*4
 
 class fusion(object):
     def __init__(self):
-        self.new_img = Image.new("RGBA", (10000, 100000))
+        self.new_img = Image.new("RGBA", (larg, hauteur))
         self.totallarg = 0
         self.totalhauteur = 0
 
-    def putpixel(self, base_img):
+    def putpixel(self, base_img,deg=0):
         base_img = Image.open(f"img\{base_img}")
+        base_img=base_img.rotate(deg)
         largeur, hauteur = base_img.size
+        if self.totallarg+largeur > larg:
+            self.totalhauteur+=512
+            self.totallarg=0
         for x in range(largeur):
             xput = self.totallarg+x
             for y in range(hauteur):
+                yput=y+self.totalhauteur
                 temp = base_img.getpixel((x, y))
-                self.new_img.putpixel((xput, y), temp)
-        self.totalhauteur += hauteur
+                self.new_img.putpixel((xput, yput), temp)
         self.totallarg += largeur
 
 
-a = fusion()
-a.putpixel("9780.png")
-a.new_img.show()
