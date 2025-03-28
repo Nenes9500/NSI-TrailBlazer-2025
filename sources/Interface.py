f719433726ecc51300efe1b5adef4f7ccf7a36ac
Nenes9500 <122723239+Nenes9500@users.noreply.github.com>
2025-03-28 12:25:34 +0100

    Update Interface.py

--- sources/Interface.py
+++ sources/Interface.py
@@ -15,71 +15,8 @@ pygame.joystick.init()
 def Start(Map):
     """This function runs the code of the editor.
     It is used to prevent the code to run on import"""
-    WINDOW_SIZE = pygame.display.get_desktop_sizes()[0]
-    WINDOW_FLAGS = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.NOFRAME  # | pygame.RESIZABLE
-
-    Gameover=False
     
-    if pygame.joystick.get_count() != 0:
-        controller = True
-        joysticks = pygame.joystick.Joystick(0)
-        joysticks.init()
-        print(f"Joystick detected: {joysticks.get_name()}")
-    else:
-        controller = False
-
-    window = pygame.display.set_mode(WINDOW_SIZE, WINDOW_FLAGS)
-
-    pygame.display.set_caption("TrailBlazer")
-    police = pygame.font.Font(None, 36)
-    policebigger = pygame.font.Font(None, 72)
-
-    background = pygame.image.load(f"Map/{Map}/{Map}.png")
-    background_size = background.get_size()
-    background = pygame.transform.scale(
-        background, (background_size[0]*1.5, background_size[1]*1.5))
-    car_img = pygame.image.load('img/voiture.png').convert_alpha()
-    car_img_size = car_img.get_size()
-    car_img = pygame.transform.scale(
-        car_img, (car_img_size[0]//4, car_img_size[1]//4))
-
-
-    car = Game(car_img, WINDOW_SIZE[0]//2, WINDOW_SIZE[1]//2)
-    car_sprites = pygame.sprite.Group()
-    car_sprites.add(car.player)
-
-    car.player.turn(-80, True)
-
-    fantome = Fantome()
-
-    chrono = Minuteur()
-    score = Score()
-    kevents = {
-        "up": [pygame.K_UP, pygame.K_z],
-        "down": [pygame.K_DOWN, pygame.K_s],
-        "left": [pygame.K_LEFT, pygame.K_q],
-        "right": [pygame.K_RIGHT, pygame.K_d],
-        "handbrake": [pygame.K_SPACE],
-        "gearup": [pygame.K_e],
-        "geardown": [pygame.K_a],
-        "leave": [pygame.K_ESCAPE]
-    }
-
-    chrono.run()
-
-    clock = pygame.time.Clock()
-    joystick_axis_0 = 0
-    joystick_axis_4 = 0
-    joystick_axis_5 = 0
-    X_button = False
-    running = True
-
-    # Couleurs
-    CENTER = (75, 75)
-    RADIUS = 75
-    BLACK = (0, 0, 0)
-    RED = (200, 0, 0)
-
+    
     class Ball(pygame.sprite.Sprite):
         def __init__(self, x, y):
             super().__init__()
@@ -89,12 +26,9 @@ def Start(Map):
 
         def update(self, x, y):
             self.rect.center = (x, y)
-    ball = Ball(car.player.position.x, car.player.position.y)
-
-    ball_sprites = pygame.sprite.Group()
-    ball_sprites.add(ball)
 
     def draw_needle(angle):
+        """This function draws the speedometer needle."""
         pygame.draw.circle(window, BLACK, CENTER, RADIUS, 3, True, True)
         pygame.draw.line(window, BLACK, (0, 75), (150, 75), 3)
 
@@ -108,9 +42,9 @@ def Start(Map):
 
         pygame.display.flip()
 
-
-    def paused(pause,fantome,Map):
-        text = policebigger.render("Save the replay",1,(255,255,255))
+    def paused(pause, fantome, Map):
+        """This function pauses the game."""
+        text = policebigger.render("Save the replay", 1, (255, 255, 255))
         text2 = policebigger.render("Load a Replay",1,(255,255,255))
         while pause:
             rect = pygame.Rect(WINDOW_SIZE[0]//2-225, WINDOW_SIZE[1]//2+100, 400, 50)
@@ -171,137 +105,136 @@ def Start(Map):
             clock.tick(15)
                 
         return (x,y)
-    car.player.position=(300, 716)
-    car.player.positionBot = car.player.position
-    while running:
-        if fantome.Play == False:
-            keys = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                car.pressed[event.key] = True
-                if event.key == pygame.K_p:
-                    p = policebigger.render(f"Pause", True, (255, 0, 0))
-                    window.blit(p, (WINDOW_SIZE[0]/2-100, WINDOW_SIZE[1]/2-50))
-                    running = paused(True,fantome,Map)
-                    if running != True and running != False:
-                        fantome.lst=running
-                        running=True
-                        fantome.Play=True
-                        car.player.speed = 0
-                        car.player.position = car.player.positionBot
-                        car.player.heading = 0
-                        car.player.turn(-80, True)
-            elif event.type == pygame.KEYUP:
-                car.pressed[event.key] = False
-
-            if controller != False:
-                if event.type == pygame.JOYAXISMOTION:
-                    if event.axis == 0:
-                        joystick_axis_0 = event.value
-                    if event.axis == 4:
-                        joystick_axis_4 = event.value
-                    if event.axis == 5:
-                        joystick_axis_5 = event.value
-
-        if abs(joystick_axis_0) > 0.1:
-            car.player.turn(1.8 * joystick_axis_0)
-
-        if abs(joystick_axis_4) > 0.1:
-            car.player.accelerate(-0.1 * (joystick_axis_4+1)/1.5)
-
-        if abs(joystick_axis_5) > 0.1:
-            car.player.accelerate(0.1 * (joystick_axis_5+1)/1.5)
-
-        if controller != False:
-            if joysticks.get_button(2):
-                X_button = True
-            else:
-                X_button = False
-        if fantome.Play == False:
-            keypresses = [k for k, v in controls.pressed.items() if v == True]
-
-        if (not any(key in kevents["up"] for key in keypresses) or joystick_axis_5 != 0) and (not any(key in kevents["down"] for key in keypresses) or joystick_axis_5 != 0):
-            if car.player.speed > 0:
-                car.player.brake(0.05)
-            elif car.player.speed < 0:
-                car.player.brake(-0.05)
-
-        for key in keypresses:
-            if key in kevents["leave"]:
-                running = False
-
-            if key in kevents["down"]:
-                car.player.accelerate(-0.1)
-
-            if key in kevents["up"]:
-                car.player.accelerate(0.1)
-
-            if key in kevents["handbrake"] or X_button == True:
-                if car.player.speed < 0:
-                    car.player.brake(-0.5)
-                else:
-                    car.player.brake(0.5)
-
-            if key in kevents["left"]:
-                car.player.turn(-1.8)
-            if key in kevents["right"]:
-                car.player.turn(1.8)
-
-        speed = police.render(
-            (f"Speed :{abs(round(car.player.speed, 1))}"), True, (255, 255, 255))
-
-        chrono.affichage()
-        minuteur = police.render(
-            (f"{chrono}"), True, (255, 255, 255))
-
-        score.calculscore(car.player.speed, chrono.timer)
-        scr = police.render(
-            (f"{score}"), True, (255, 255, 255))
-
-        car_sprites.update()
-
-        window.blit(background, (-car.player.position.x, - car.player.position.y))
-        window.blit(speed, (25, 100))
-        window.blit(minuteur, ((WINDOW_SIZE[0]//2)-75, 10))
-        window.blit(scr, (25, 150))
-        car_sprites.draw(window)
-
-        if fantome.Play == False:
-            fantome.add_movement(keypresses)
-        else:
-            keypresses = fantome.playing()
-            
-        draw_needle(abs(car.player.speed))
-
-        if controller == True:
-            joysticks.rumble(abs(car.player.speed/car.player.maxspeedfront*0.1),
-                            abs(car.player.speed/car.player.maxspeedfront*0.1), 1)
-            
-        x, y = int(car.player.position.x+(WINDOW_SIZE[0]//2)), int(car.player.position.y+(WINDOW_SIZE[1]//2))
 
 
+    WINDOW_SIZE = pygame.display.get_desktop_sizes()[0]
+    WINDOW_FLAGS = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.NOFRAME  # | pygame.RESIZABLE
 
-        color = background.get_at((x, y))
-        if color ==(0,162,232,255):
-            chrono = Minuteur()
-            chrono.run()
-            chrono.timer=True
-        elif color ==(237,28,36,255):
-            chrono.timere=False
-        elif color.g > 150 and color.r < 100 and color.b < 100:  
-            running=False
-            Gameover=True
-            fantome.lst=[]
-            fantome.i=0
-    # clock.tick_busy_loop(60)
-    if Gameover==True :
-        gameover = policebigger.render("Game Over", True, (255, 0, 0))
-        window.blit(gameover, (WINDOW_SIZE[0]//2-150, WINDOW_SIZE[1]//2-50))
-        pygame.display.update()
-        time.sleep(2)
+    Gameover=False
+
+    window = pygame.display.set_mode(WINDOW_SIZE, WINDOW_FLAGS)
+
+    pygame.display.set_caption("TrailBlazer")
+    police = pygame.font.Font(None, 36)
+    policebigger = pygame.font.Font(None, 72)
+
+    background = pygame.image.load(f"Map/{Map}/{Map}.png")
+    background_size = background.get_size()
+    background = pygame.transform.scale(
+        background, (background_size[0]*1.5, background_size[1]*1.5))
+    car_img = pygame.image.load('img/voiture.png').convert_alpha()
+    car_img_size = car_img.get_size()
+    car_img = pygame.transform.scale(
+        car_img, (car_img_size[0]//4, car_img_size[1]//4))
+
+
+    car = Game(car_img, WINDOW_SIZE[0]//2, WINDOW_SIZE[1]//2)
+    car_sprites = pygame.sprite.Group()
+    car_sprites.add(car.player)
+
+    car.player.turn(-80, True)
+
+    fantome = Fantome()
+
+    chrono = Minuteur()
+    score = Score()
+
+    chrono.run()
+
+    clock = pygame.time.Clock()
+    running = True
+
+    # Couleurs
+    CENTER = (75, 75)
+    RADIUS = 75
+    BLACK = (0, 0, 0)
+    RED = (200, 0, 0)
+
+    ball = Ball(car.player.position.x, car.player.position.y)
+
+    ball_sprites = pygame.sprite.Group()
+    ball_sprites.add(ball)
 
 
     
+    car.player.position=(300, 716)
+    car.player.positionBot = car.player.position
+    try:
+        while running:
+            if fantome.Play == False:
+                keys = pygame.key.get_pressed()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    car.pressed[event.key] = True
+                    if event.key == pygame.K_p:
+                        p = policebigger.render(f"Pause", True, (255, 0, 0))
+                        window.blit(p, (WINDOW_SIZE[0]/2-100, WINDOW_SIZE[1]/2-50))
+                        running = paused(True,fantome,Map)
+                        if running != True and running != False:
+                            fantome.lst=running
+                            running=True
+                            fantome.Play=True
+                            car.player.speed = 0
+                            car.player.position = car.player.positionBot
+                            car.player.heading = 0
+                            car.player.turn(-80, True)
+                elif event.type == pygame.KEYUP:
+                    car.pressed[event.key] = False
+
+            if fantome.Play == False:
+                keypresses = [k for k, v in controls.pressed.items() if v == True]
+
+            speed = police.render(
+                (f"Speed :{abs(round(car.player.speed, 1))}"), True, (255, 255, 255))
+
+            chrono.affichage()
+            minuteur = police.render(
+                (f"{chrono}"), True, (255, 255, 255))
+
+            score.calculscore(car.player.speed, chrono.timer)
+            scr = police.render(
+                (f"{score}"), True, (255, 255, 255))
+
+            car_sprites.update()
+
+            window.blit(background, (-car.player.position.x, - car.player.position.y))
+            window.blit(speed, (25, 100))
+            window.blit(minuteur, ((WINDOW_SIZE[0]//2)-75, 10))
+            window.blit(scr, (25, 150))
+            car_sprites.draw(window)
+
+            if fantome.Play == False:
+                fantome.add_movement(keypresses)
+            else:
+                keypresses = fantome.playing()
+                
+            draw_needle(abs(car.player.speed))
+
+            x, y = int(car.player.position.x+(WINDOW_SIZE[0]//2)), int(car.player.position.y+(WINDOW_SIZE[1]//2))
+
+            color = background.get_at((x, y))
+            if color ==(0,162,232,255):
+                chrono = Minuteur()
+                chrono.run()
+                chrono.timer=True
+            elif color ==(237,28,36,255):
+                chrono.timer=False
+            elif color.g > 150 and color.r < 100 and color.b < 100:  
+                running=False
+                Gameover=True
+                fantome.lst=[]
+                fantome.i=0
+        # clock.tick_busy_loop(60)
+        if Gameover==True :
+            gameover = policebigger.render("Game Over", True, (255, 0, 0))
+            window.blit(gameover, (WINDOW_SIZE[0]//2-150, WINDOW_SIZE[1]//2-50))
+            pygame.display.update()
+            time.sleep(2)
+    except Exception as e:
+        print(f"An error occured: {e}")
+        pygame.quit()
+        raise e
+
 pygame.quit()
